d4fefbf7aa89735b126f654ebde797587b411265
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-12 01:20:32 +0300

    [enums] declared enums for UserRoles and OrderStatuses

---
+++ models/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class UserRole(Enum):
+    regular = "User"
+    manager = "Store Manager"
+    dispatcher = "Dispatcher"
+
+
+class OrderStatus(Enum):
+    new = "Waiting to process the order."
+    dispatched = "The order is collected by dispatcher."
+    shipped = "The order is shipped."
+    received = "The order is received by customer."
