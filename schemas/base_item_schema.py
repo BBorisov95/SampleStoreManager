de177fc66bddb3432bf7227465d1d6a862228030
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-13 23:32:49 +0300

    [base_item_schema] added custom validators

--- schemas/base_item_schema.py
+++ schemas/base_item_schema.py
@@ -1,12 +1,14 @@
 from marshmallow import Schema, fields
 
-from utils.validators.price_validator import validate_price
+from utils.validators.item_validators.item_names_validator import validate_item_name
+from utils.validators.item_validators.part_number_validator import validate_part_number
+from utils.validators.item_validators.price_validator import validate_price
 
 
 class BaseItemSchema(Schema):
-    name = fields.String(required=True)
+    name = fields.String(required=True, validate=validate_item_name)
     price = fields.Float(required=True, validate=validate_price)
-    part_number = fields.String(required=True)
-    ean = fields.Float(required=True)
+    part_number = fields.String(required=True, validate=validate_part_number)
+    ean = fields.String(required=True)
     category = fields.String(required=True)
     specs = fields.Dict()
