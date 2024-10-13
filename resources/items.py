a0dc42b32f45af5ef7c3d45fab262ea461ac86e3
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-14 00:48:57 +0300

    [items] new resource GetItem which implements different item visualizations based on user role

--- resources/items.py
+++ resources/items.py
@@ -5,7 +5,7 @@ from managers.authenticator import auth
 from managers.item import ItemManager
 from models.enums import UserRole
 from schemas.request.item import ItemCreationSchema
-from schemas.response.item import ItemResponseSchema
+from schemas.response.item import ItemResponseSchema, ItemResponseManagersSchema, ItemResponseDispatcherSchema
 from utils.decorators import permission_required, validate_schema
 
 
@@ -26,3 +26,18 @@ class DeleteItem(Resource):
     def delete(self, item_id: int):
         ItemManager.delete_item(item_id)
         return {}, 204
+
+
+class GetItem(Resource):
+    @auth.login_required
+    def get(self, item_id: int):
+        user = auth.current_user()
+
+        requested_item = ItemManager.get_item(item_id)
+
+        if user.role == UserRole.regular:
+            return {"item": ItemResponseSchema().dump(requested_item)}, 200
+        if user.role == UserRole.manager:
+            return {"item": ItemResponseManagersSchema().dump(requested_item)}, 200
+        if user.role == UserRole.dispatcher:
+            return {"item": ItemResponseDispatcherSchema().dump(requested_item)}, 200
