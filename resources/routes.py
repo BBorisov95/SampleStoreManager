256f54758be582b1b234f31f48faf9c85ab70217
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-13 23:34:13 +0300

    [routes] added create-item endpoint

--- resources/routes.py
+++ resources/routes.py
@@ -1,3 +1,8 @@
 from resources.authenticator import RegisterUser, LoginUser
+from resources.items import CreateItem
 
-routes = ((RegisterUser, "/register"), (LoginUser, "/login"))
+routes = (
+    (RegisterUser, "/register"),
+    (LoginUser, "/login"),
+    (CreateItem, "/management/create-item"),
+)
