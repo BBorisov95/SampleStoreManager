2248ba2a1b2d9dc6b1162209cbfe95584855b6f2
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-13 19:05:05 +0300

    [authenticator] added resources allowing post request for registration and login

---
+++ resources/authenticator.py
@@ -0,0 +1,22 @@
+from flask import request
+from flask_restful import Resource
+
+from managers.user import UserManager
+from schemas.request.user import UserRegisterSchema, UserLoginSchema
+from utils.decorators import validate_schema
+
+
+class RegisterUser(Resource):
+    @validate_schema(UserRegisterSchema)
+    def post(self):
+        data = request.get_json()
+        token = UserManager.create_user(data)
+        return {"token": token}, 201
+
+
+class LoginUser(Resource):
+    @validate_schema(UserLoginSchema)
+    def post(self):
+        data = request.get_json()
+        token = UserManager.login(data)
+        return {"token": token}, 200
