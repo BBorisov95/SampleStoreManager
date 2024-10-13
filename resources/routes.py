ae13e795ee2d570f50755a08068f9ee23ac00a6b
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-14 01:16:45 +0300

    [routes] added route to get items per category

--- resources/routes.py
+++ resources/routes.py
@@ -1,9 +1,11 @@
 from resources.authenticator import RegisterUser, LoginUser
-from resources.items import CreateItem, DeleteItem
+from resources.items import CreateItem, DeleteItem, GetItem, GetItemsFromCategory
 
 routes = (
     (RegisterUser, "/register"),
     (LoginUser, "/login"),
     (CreateItem, "/management/item/create-item"),
-    (DeleteItem, "/management/item/delete-item/<int:item_id>")
+    (DeleteItem, "/management/item/delete-item/<int:item_id>"),
+    (GetItem, "/item/get-item/<int:item_id>"),
+    (GetItemsFromCategory, "/item/category/<string:category_name>")
 )
