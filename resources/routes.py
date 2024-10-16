2e1ade4121c566c3a3f96806bb23a7bc16c230bb
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-17 00:43:24 +0300

    [routes] routes for update and restock items

--- resources/routes.py
+++ resources/routes.py
@@ -1,11 +1,20 @@
 from resources.authenticator import RegisterUser, LoginUser
-from resources.items import CreateItem, DeleteItem, GetItem, GetItemsFromCategory
+from resources.items import (
+    CreateItem,
+    DeleteItem,
+    GetItem,
+    GetItemsFromCategory,
+    UpdateItem,
+    RestockItems,
+)
 
 routes = (
     (RegisterUser, "/register"),
     (LoginUser, "/login"),
     (CreateItem, "/management/item/create-item"),
     (DeleteItem, "/management/item/delete-item/<int:item_id>"),
+    (UpdateItem, "/management/item/update-item"),
+    (RestockItems, "/management/items/restock"),
     (GetItem, "/item/get-item/<int:item_id>"),
-    (GetItemsFromCategory, "/item/category/<string:category_name>")
+    (GetItemsFromCategory, "/items/category/<string:category_name>"),
 )
