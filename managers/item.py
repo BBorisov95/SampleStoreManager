c3f844205e76433d90c7dc7352338751f8cd09ab
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-17 00:41:26 +0300

    [item] created update_item_fields/restock/custom raise_common_errors method. + refactor

--- managers/item.py
+++ managers/item.py
@@ -1,5 +1,5 @@
+from werkzeug import exceptions as werkzeug_exceptions
 from werkzeug.exceptions import NotFound
-
 from db import db
 from models import ItemModel
 from utils.db_handler import do_commit
@@ -27,7 +27,7 @@ class ItemManager:
             db.select(ItemModel).filter_by(id=item_id)
         ).scalar()
         if not requested_item:
-            raise NotFound("The item which you search is not existing!")
+            ItemManager.raises_common_errors(NotFound, raises_for="item")
         db.session.delete(requested_item)
         db.session.flush()
 
@@ -41,13 +41,33 @@ class ItemManager:
         ...
 
     @staticmethod
-    def restock(item_id: str, pcs: int):
+    def update_item_fields(data_to_change: dict):
+        """
+        Handle the logic of updating any files except specs
+        :return: None
+        """
+        requested_item_id = data_to_change.get("prod_id")
+        requested_item = db.session.execute(
+            db.select(ItemModel).filter_by(id=requested_item_id)
+        ).scalar()
+        if not requested_item:
+            ItemManager.raises_common_errors(NotFound, raises_for="item")
+        del data_to_change['prod_id']  # renamed schema's id field to prod_id to not shallow build in id
+        data_to_change['id'] = requested_item_id
+        db.session.bulk_update_mappings(ItemModel, [data_to_change])
+        db.session.flush()
+
+    @staticmethod
+    def restock(data: dict):
         """
         Increase/decrease product quantity on stock
-        :param item_id: item id
-        :param pcs: negative or positive number
-        :return: updated stock value of item
+        :param data: item data
+        :return: none -> updated stock value of item
         """
+        for item in data.get('items'):
+            item_to_restock = ItemManager.get_item(item.get('prod_id'))
+            item_to_restock.stocks += item.get('stock')
+        db.session.flush()
 
     @staticmethod
     def get_item(item_id: int):
@@ -59,7 +79,7 @@ class ItemManager:
         item = db.session.execute(db.select(ItemModel).filter_by(id=item_id)).scalar()
 
         if not item:
-            raise NotFound("The item which you search is not existing!")
+            ItemManager.raises_common_errors(NotFound, "item")
         if item.stocks <= 0:
             raise NotFound("The requested item is currently not available for sale!")
 
@@ -72,13 +92,22 @@ class ItemManager:
         :param cat_name: category name
         :return:
         """
-        items = db.session.execute(
-            db.select(ItemModel).filter_by(category=cat_name)
-        ).scalars().all()
+        items = (
+            db.session.execute(db.select(ItemModel).filter_by(category=cat_name))
+            .scalars()
+            .all()
+        )
 
         if not items:
-            raise NotFound(
-                "The requested category does not exist or does not contain items!"
-            )
+            ItemManager.raises_common_errors(NotFound, "category")
 
         return items
+
+    @staticmethod
+    def raises_common_errors(error_to_raise: werkzeug_exceptions, raises_for: str):
+        if raises_for == "item":
+            raise error_to_raise("The item which you search is not existing!")
+        if raises_for == "category":
+            raise error_to_raise(
+                "The requested category does not exist or does not contain items!"
+            )
