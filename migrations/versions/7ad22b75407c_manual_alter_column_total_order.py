567671e8b1035072758a6507c86a9d1a7cacf2b3
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-19 02:50:14 +0300

    alter total order money with default value

---
+++ migrations/versions/7ad22b75407c_manual_alter_column_total_order.py
@@ -0,0 +1,31 @@
+"""manual alter column total_order for some reason alembic does not found default=0, server_default
+
+Revision ID: 7ad22b75407c
+Revises: df571be6c40c
+Create Date: 2024-10-18 23:43:53.154401
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7ad22b75407c"
+down_revision = "df571be6c40c"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("orders", schema=None) as batch_op:
+        batch_op.alter_column(
+            "total_order", server_default="0", existing_type=sa.Float(), nullable=False
+        )
+
+
+def downgrade():
+    with op.batch_alter_table("orders", schema=None) as batch_op:
+        batch_op.alter_column(
+            "total_order", server_default=None, existing_type=sa.Float(), nullable=False
+        )
