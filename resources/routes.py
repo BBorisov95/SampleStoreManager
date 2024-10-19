f5bfdfdda5c82955bb0ec545c72768af734fa225
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-19 03:13:13 +0300

    [routes] added two new endpoints for ShowCountry and PlaceOrders

--- resources/routes.py
+++ resources/routes.py
@@ -1,3 +1,4 @@
+from resources.allowed_countries import ShowCountries
 from resources.authenticator import RegisterUser, LoginUser
 from resources.items import (
     CreateItem,
@@ -8,6 +9,7 @@ from resources.items import (
     RestockItems,
     UpdateSpecs,
 )
+from resources.orders import PlaceOrder
 
 routes = (
     (RegisterUser, "/register"),
@@ -15,8 +17,10 @@ routes = (
     (CreateItem, "/management/item/create-item"),
     (DeleteItem, "/management/item/delete-item/<int:item_id>"),
     (UpdateItem, "/management/item/update-item"),
-    (UpdateSpecs, "/data-entry/item/update-item-spec"), # TODO TEST
+    (UpdateSpecs, "/data-entry/item/update-item-spec"),  # TODO TEST
     (RestockItems, "/management/items/restock"),
     (GetItem, "/item/get-item/<int:item_id>"),
     (GetItemsFromCategory, "/items/category/<string:category_name>"),
+    (ShowCountries, "/show-countries"),
+    (PlaceOrder, "/item/purchase"),
 )
