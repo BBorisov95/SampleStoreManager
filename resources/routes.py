caed5caa5f7fed683d387785b349e2957ac1cd35
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-20 22:05:59 +0300

    [routes] new endpoints for country CRU methods.

--- resources/routes.py
+++ resources/routes.py
@@ -1,4 +1,4 @@
-from resources.allowed_countries import ShowCountries
+from resources.allowed_countries import Countries, CountiesCreate, CountiesUpdate
 from resources.authenticator import RegisterUser, LoginUser
 from resources.items import (
     CreateItem,
@@ -17,10 +17,12 @@ routes = (
     (CreateItem, "/management/item/create-item"),
     (DeleteItem, "/management/item/delete-item/<int:item_id>"),
     (UpdateItem, "/management/item/update-item"),
-    (UpdateSpecs, "/data-entry/item/update-item-spec"),  # TODO TEST
+    (UpdateSpecs, "/data-entry/item/update-item-spec"),
     (RestockItems, "/management/items/restock"),
     (GetItem, "/item/get-item/<int:item_id>"),
     (GetItemsFromCategory, "/items/category/<string:category_name>"),
-    (ShowCountries, "/show-countries"),
+    (Countries, "/show-countries"),
+    (CountiesCreate, "/management/create-country"),
+    (CountiesUpdate, "/management/update-country-taxes"),
     (PlaceOrder, "/item/purchase"),
 )
