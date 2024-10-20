ebef0bdc5761fd6fa2c8f9d83ce9e3eab07709e0
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-20 22:00:10 +0300

    [allowed_countries] created CRU resources for counties

--- resources/allowed_countries.py
+++ resources/allowed_countries.py
@@ -3,25 +3,49 @@ This file is like a mapper for which countries we can deliver
 Will be exposed to endpoint
 """
 
+from flask import request
 from flask_restful import Resource
 
 from schemas.response.allowed_countries import AllowedCountriesResponseSchema
+from schemas.request.country import CountryCreateAndUpdateSchema
+from managers.authenticator import auth
+from managers.country import CountryManager
+from models.enums import UserRole
+from utils.decorators import validate_schema, permission_required
 
-ALLOWED_COUNTRIES = {"bulgaria": "bgr", "greece": "grc"} # TODO DB!
 
+class Countries(Resource):
 
-class ShowCountries(Resource):
+    @staticmethod
+    def get():
+        all_countries = CountryManager.get_all_allowed_countries()
 
-    def get(self):
-        formatted_countries = {
-            country.title(): code.upper() for country, code in ALLOWED_COUNTRIES.items()
+        formatted_countries: dict = {
+            country.title(): code.upper() for country, code in all_countries
         }
 
-        response = AllowedCountriesResponseSchema().dump({
-            "allowed_countries_to_deliver": formatted_countries
-        })
-        return {
-            "countries": response
+        response: dict = AllowedCountriesResponseSchema().dump(
+            {"allowed_countries_to_deliver": formatted_countries}
+        )
+        return {"countries": response}, 200
 
 
-        }, 200
+class CountiesCreate(Resource):
+    @auth.login_required
+    @permission_required(UserRole.manager)
+    @validate_schema(CountryCreateAndUpdateSchema)
+    def post(self):
+        data: dict = request.get_json()
+        CountryManager.create_new_country(data)
+        return {}, 201
+
+
+class CountiesUpdate(Resource):
+
+    @auth.login_required
+    @permission_required(UserRole.manager)
+    @validate_schema(CountryCreateAndUpdateSchema)
+    def put(self):
+        data: dict = request.get_json()
+        CountryManager.update_delivery_taxes(data)
+        return {}, 201
