c592b118901d1858acad4e4a7ae82fc5a1cf77be
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-20 22:00:35 +0300

    [base_order_address_Schema] modify to access street and building

--- schemas/base_order_address_schema.py
+++ schemas/base_order_address_schema.py
@@ -9,4 +9,6 @@ from schemas.validators.order_validators.delivery_address_validator import (
 class DeliveryAddressSchema(Schema):
     to_country = fields.String(required=True, validate=validate_country)
     to_city = fields.String(required=True, validate=validate_city)
+    to_street_address = fields.String(required=True)
+    to_building_number = fields.Integer(required=True)
     to_zipcode = fields.String(required=True, validate=validate_postal_code)
