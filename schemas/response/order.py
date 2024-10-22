df6300b74c73eb57270621098a7a0351608b6c6e
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-22 21:05:47 +0300

    [order] modded schemas/response delivery_to field to be required

--- schemas/response/order.py
+++ schemas/response/order.py
@@ -8,7 +8,7 @@ from schemas.base_order_address_schema import DeliveryAddressSchema
 class OrderResponseSchema(Schema):
 
     id = fields.Integer()
-    delivery_to = fields.Nested(DeliveryAddressSchema)
+    delivery_to = fields.Nested(DeliveryAddressSchema, required=True)
     status = EnumField(OrderStatus, by_value=True)
     delivery_type = EnumField(DeliveryType, by_value=True)
     payment_status = EnumField(PaymentStatus, by_value=True)
