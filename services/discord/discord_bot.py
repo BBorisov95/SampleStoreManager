fa8a91d2958f6b4d2403b4df081cd87e8481f100
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-27 16:53:10 +0200

    [discord_bot] modded send_msg to have custom msg based on passed args

--- services/discord/discord_bot.py
+++ services/discord/discord_bot.py
@@ -15,10 +15,15 @@ class DiscordBot:
             "Authorization": f"Bot {self.bot_id}",
         }
 
-    def send_msg(self, order_id: int):
-        json_payload = {
-            "content": f"Order ID: {order_id} is collected and dispatched to desired location."
+    def send_msg(self, order_id: int, action: str):
+        action_msg_mapper = {
+            "dispatch": "is collected and dispatched to desired location."
             f" By making PUT request on this link: {config('dns')}:/dispatcher/approve-shipped/{order_id} you will changed the order status to shipped!",
+            "payment_success": "is successfully payed by user!",
+            "payment_failed": "failed to be payed by user! Please contact him!",
+        }
+        json_payload = {
+            "content": f"Order ID: {order_id} {action_msg_mapper[action]}",
         }
         requests.post(
             f"https://discord.com/api/v9/channels/{self.channel_id}/messages",
