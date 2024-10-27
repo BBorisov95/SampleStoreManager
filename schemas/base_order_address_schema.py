67a8adb6d0442c20630cf61d4388bf2be47dad89
BBorisov95 <b.r.borisov95@gmail.com>
2024-10-27 16:50:07 +0200

    [base_order_address_schema] modded last_update_by to not be required on that level

--- schemas/base_order_address_schema.py
+++ schemas/base_order_address_schema.py
@@ -1,4 +1,5 @@
 from marshmallow import Schema, fields
+
 from schemas.validators.order_validators.delivery_address_validator import (
     validate_country,
     validate_city,
@@ -12,4 +13,4 @@ class DeliveryAddressSchema(Schema):
     to_street_address = fields.String(required=True)
     to_building_number = fields.Integer(required=True)
     to_zipcode = fields.String(required=True, validate=validate_postal_code)
-    last_update_by = fields.Integer(required=True)
+    last_update_by = fields.Integer()
